90a0b10fc37c8c760a256da285c313b0f13d8c59
RAMraghuram <ram309591@gmail.com>
2022-07-12 10:33:10 +0530

    first commit

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by Raghuram on 12/07/22.
+//
+
+import Foundation
+
+public class Service{
+    private init(){}
+    public static func doSomething() -> String{
+        return "Did something in this framework."
+    }
+    
+}
